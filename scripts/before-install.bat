c5ab0bc2cd26bcdcd9436df3e869c899b1020ed1
Ryan Shivashankar <TheArbiter@users.noreply.github.com>
2021-06-27 21:28:15 +1000

    changed appspec.yml file a bit

--- scripts/before-install.bat
+++ scripts/before-install.bat
@@ -1,7 +1,11 @@
-set FOLDER=%HOMEDRIVE%\temp\COMP9447CodeDeploy
+set FOLDER=%HOMEDRIVE%\temp\CodeDeployExample
 
 if exist %FOLDER% (
   rd /s /q "%FOLDER%"
 )
 
 mkdir %FOLDER%
+
+REM Install Internet Information Server (IIS).
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
