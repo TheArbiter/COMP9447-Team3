07bd887ed3aba892a709aac7a93f990053ba71e5
Arpit Mathur <20039537+arpitmathur1@users.noreply.github.com>
2021-07-27 16:48:11 +1000

    sonar issue push

---
+++ sonarqube-scanner-maven/maven-basic/src/main/java/com/acme/basic/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.acme.basic;
+
+public class HelloWorld {
+
+  void sayHello() {
+    System.out.println("Hello World!");
+    System.out.println("Hello 1");
+  }
+
+  void notCovered() {
+    System.out.println("This method is not covered by unit tests");
+  }
+
+
+}
